e07c737aaddc266d5dfd569af8a1e3ad54103048
Eli Maczuzak <eli@elis-mac.local>
2016-03-12 17:21:13 -0600

    Created the rest of the views and chose a color scheme

--- HW2/addEvent.swift
+++ HW2/addEvent.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import UIKit
+import EventKit
+
+class addEvent: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
--- HW2/deleteEvent.swift
+++ HW2/deleteEvent.swift
@@ -1,24 +1,24 @@
 //
-//  ViewController.swift
+//  deleteEvent.swift
 //  HW2
 //
 //  Created by Eli Maczuzak on 3/12/16.
 //  Copyright © 2016 Eli Maczuzak. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class deleteEvent: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
-
